5f5c11f1082882a650cc925bef2c206eb6925e12
Kyle <wong.kyle5@gmail.com>
2013-07-02 19:44:12 -0700

    worked for 3 pdb files in r_test

--- align_all.py
+++ align_all.py
@@ -8,29 +8,29 @@ import chimera
 from os import chdir, listdir
 from chimera import *
 
-chdir("receptors") # change to the receptors file directory
+#chdir("receptors") # change to the receptors file directory
 for pdb in listdir("."):
         if pdb.endswith("_aligned.pdb"):
+		continue
+	if pdb.endswith("_noH.pdb"):
+		continue
+        if not pdb.endswith("_mod2.pdb"): #originally was .pdb before pdb2pqr.py
                 continue
-        if pdb.endswith("_noH.pdb"):
-                continue
-        if not pdb.endswith(".pdb"):
-                continue
-        if pdb == "1gzm.pdb": # don't need to do the pdb used for align
-                continue
-        runCommand("open " + pdb)
-        runCommand("open 1gzm.pdb")
-        runCommand("mmaker #1 #0")
-        runCommand("write relative #1 #0 " + pdb[:-4] + "_aligned.pdb")
+	#if pdb == "1gzm.pdb": # don't need to do the pdb used for align
+	#	continue
+	runCommand("open " + pdb)
+	runCommand("open 1gzm.pdb")
+	runCommand("mmaker #1 #0")
+	runCommand("write relative #1 #0 " + pdb[:-4] + "_aligned.pdb")
         runCommand("close all")
-        pdb = pdb[:-4] + "_aligned.pdb"
+	pdb = pdb[:-4] + "_aligned.pdb"
 
-        runCommand("open " + pdb)
-        from DockPrep import prep
-        models = chimera.openModels.list(modelTypes=[chimera.Molecule])
-        prep(models)
-        from WriteMol2 import writeMol2
-        writeMol2(models, pdb[:-4]+ "_charged.mol2")
+	runCommand("open " + pdb)
+	from DockPrep import prep
+	models = chimera.openModels.list(modelTypes=[chimera.Molecule])
+	prep(models)
+	from WriteMol2 import writeMol2
+	writeMol2(models, pdb[:-4]+ "_charged.mol2")
 
         runCommand("select H")
         runCommand("del sel")
