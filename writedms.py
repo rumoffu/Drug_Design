0239ebc2be25664b1324e8910d5e456c44f9b0c6
Kyle <wong.kyle5@gmail.com>
2013-07-02 18:39:30 -0700

    Update writedms.py

--- writedms.py
+++ writedms.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-# run with
-# chimera --nogui olfr1392_1u19_noH.pdb writedms.py
-#chimera --nogui pdb:3D7F_withoutLigand writedms.py
+# run with 
+## chimera --nogui olfr1392_1u19_noH.pdb writedms.py
+## chimera --nogui pdb:3D7F_withoutLigand writedms.py 
+# chimera --nogui Q9I8C1_27_noH.pdb writedms.py 
 # where this py file is named writedms.py
-# it would run chimera without gui on the file 3D7F_withoutLigand.pdb 
+# it would run chimera without gui on the file 3D7F_withoutLigand.pdb  
 # and it would output to a file called 3D7F_without_ligand.dms
 
 from chimera import runCommand, openModels, MSMSModel
@@ -14,4 +14,6 @@ surf = openModels.list(modelTypes=[MSMSModel])[0]
 # write DMS
 from WriteDMS import writeDMS
 #writeDMS(surf, "3D7F_rec_noH.dms")
-writeDMS(surf, "olfr1392_1u19_noH.dms")
+#writeDMS(surf, "olfr1392_1u19_noH.dms")
+#writeDMS(surf, "Q9I8C1_27_aligned_noH.dms")
+writeDMS(surf, "rec.dms")
